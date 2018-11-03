fbb64f315b555b7f63f8bda063255e215e548855
Dobriyan Benov <benov@benov.org>
2018-11-03 20:24:29 +0200

    Circle and Intersection functions

--- MathLib/Geometry.cs
+++ MathLib/Geometry.cs
@@ -257,5 +257,110 @@ namespace Benov.MathLib
             degBearing = Core.FitAngle(degBearing);
             return degBearing;
         }
+
+        // Find the point of intersection between
+        // the lines p1 --> p2 and p3 --> p4.
+        private void FindIntersection(
+            PointF p1, PointF p2, PointF p3, PointF p4,
+            out bool lines_intersect, out bool segments_intersect,
+            out PointF intersection,
+            out PointF close_p1, out PointF close_p2)
+        {
+            // Get the segments' parameters.
+            float dx12 = p2.X - p1.X;
+            float dy12 = p2.Y - p1.Y;
+            float dx34 = p4.X - p3.X;
+            float dy34 = p4.Y - p3.Y;
+
+            // Solve for t1 and t2
+            float denominator = (dy12 * dx34 - dx12 * dy34);
+
+            float t1 =
+                ((p1.X - p3.X) * dy34 + (p3.Y - p1.Y) * dx34)
+                    / denominator;
+            if (float.IsInfinity(t1))
+            {
+                // The lines are parallel (or close enough to it).
+                lines_intersect = false;
+                segments_intersect = false;
+                intersection = new PointF(float.NaN, float.NaN);
+                close_p1 = new PointF(float.NaN, float.NaN);
+                close_p2 = new PointF(float.NaN, float.NaN);
+                return;
+            }
+            lines_intersect = true;
+
+            float t2 =
+                ((p3.X - p1.X) * dy12 + (p1.Y - p3.Y) * dx12)
+                    / -denominator;
+
+            // Find the point of intersection.
+            intersection = new PointF(p1.X + dx12 * t1, p1.Y + dy12 * t1);
+
+            // The segments intersect if t1 and t2 are between 0 and 1.
+            segments_intersect =
+                ((t1 >= 0) && (t1 <= 1) &&
+                 (t2 >= 0) && (t2 <= 1));
+
+            // Find the closest points on the segments.
+            if (t1 < 0)
+            {
+                t1 = 0;
+            }
+            else if (t1 > 1)
+            {
+                t1 = 1;
+            }
+
+            if (t2 < 0)
+            {
+                t2 = 0;
+            }
+            else if (t2 > 1)
+            {
+                t2 = 1;
+            }
+
+            close_p1 = new PointF(p1.X + dx12 * t1, p1.Y + dy12 * t1);
+            close_p2 = new PointF(p3.X + dx34 * t2, p3.Y + dy34 * t2);
+        }
+
+        // Find a circle through the three points.
+        private void FindCircle(PointF a, PointF b, PointF c,
+            out PointF center, out float radius)
+        {
+            // Get the perpendicular bisector of (x1, y1) and (x2, y2).
+            float x1 = (b.X + a.X) / 2;
+            float y1 = (b.Y + a.Y) / 2;
+            float dy1 = b.X - a.X;
+            float dx1 = -(b.Y - a.Y);
+
+            // Get the perpendicular bisector of (x2, y2) and (x3, y3).
+            float x2 = (c.X + b.X) / 2;
+            float y2 = (c.Y + b.Y) / 2;
+            float dy2 = c.X - b.X;
+            float dx2 = -(c.Y - b.Y);
+
+            // See where the lines intersect.
+            bool lines_intersect, segments_intersect;
+            PointF intersection, close1, close2;
+            FindIntersection(
+                new PointF(x1, y1), new PointF(x1 + dx1, y1 + dy1),
+                new PointF(x2, y2), new PointF(x2 + dx2, y2 + dy2),
+                out lines_intersect, out segments_intersect,
+                out intersection, out close1, out close2);
+            if (!lines_intersect)
+            {
+                center = new PointF(0, 0);
+                radius = 0;
+            }
+            else
+            {
+                center = intersection;
+                float dx = center.X - a.X;
+                float dy = center.Y - a.Y;
+                radius = (float)Math.Sqrt(dx * dx + dy * dy);
+            }
+        }
     }
 }
